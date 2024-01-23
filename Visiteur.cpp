f82bec247368d93ddb9bf5c6629d0706b8b9d49d
Hugo_G_Laptop <hgenillier@ensc.fr>
2024-01-23 15:09:46 +0100

    Classe Visiteur v1

---
+++ Visiteur.cpp
@@ -0,0 +1,16 @@
+#include "Visiteur.h"
+#include <iostream>
+using namespace std;
+
+// Constructeur
+Visiteur::Visiteur(std::string nom) : Nom(nom), TempsAttendu(0) {}
+
+// Méthode publique
+void Visiteur::AfficherDetails() const {
+        cout << "Nom : " << Nom << ", Temps total d'attente : " << TempsAttendu << endl;
+    }
+
+// Destructeur
+    Visiteur::~Visiteur(){}
+
+
---
+++ Visiteur.h
@@ -0,0 +1,25 @@
+// Personne.hpp
+
+#ifndef VISITEUR_HPP
+#define VISITEUR_HPP
+#include <string>
+
+class Visiteur {
+public:
+    // Constructeur
+    Visiteur(std::string nom);
+
+    // Méthode publique
+    void AfficherDetails() const;
+
+    // Destructeur
+    ~Visiteur();
+
+private:
+    // Membres privés
+    std::string Nom;
+    int TempsAttendu;
+};
+
+#endif // PERSONNE_HPP
+
