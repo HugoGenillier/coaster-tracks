cb91fa4fb1e92283af4acb079d55ea9a2f14d0f4
Hugo_G_Laptop <hgenillier@ensc.fr>
2024-02-06 16:09:06 +0100

    Horloge modif et thread

--- Horloge.cpp
+++ Horloge.cpp
@@ -3,7 +3,25 @@
 #include <thread>
 #include <chrono>
 
-Horloge::Horloge() : heureActuelle(std::chrono::system_clock::now()) {}
+Horloge::Horloge() {
+	// Récupérer la date et l'heure actuelle du système
+	std::time_t maintenant = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+	// Convertir la date et l'heure actuelle en une structure tm (pour accéder aux informations sur l'heure)
+	std::tm ltm;
+	localtime_s(&ltm, &maintenant); // Utilisation de localtime_s au lieu de localtime
+
+	// Initialiser l'heure de l'horloge à 00h00
+	ltm.tm_hour = 0;
+	ltm.tm_min = 0;
+	ltm.tm_sec = 0;
+
+	// Convertir la structure tm en time_t
+	std::time_t debutDuJour = std::mktime(&ltm);
+
+	// Convertir le time_t en un objet time_point
+	heureActuelle = std::chrono::system_clock::from_time_t(debutDuJour);
+}
 
 void Horloge::avancerTemps(int heures) {
 	heureActuelle += std::chrono::hours(heures);
--- main.cpp
+++ main.cpp
@@ -4,25 +4,42 @@
 #include <queue>
 #include "Structure.hpp"
 #include "Horloge.h"
+#include <thread>
 using namespace std;
 
 #include <iostream>
 #include "Horloge.h" // Inclure le fichier d'en-tête de la classe Horloge
 
+
+void avancerTemps(Horloge& horloge) {
+	// Durée entre chaque avancement du temps (10 secondes dans cet exemple)
+	std::chrono::seconds dureeAvancement(10);
+
+	while (true) {
+		// Avancer le temps de 1 heure
+		horloge.avancerTemps(1);
+
+		// Afficher l'heure actuelle
+		std::cout << "Heure actuelle : " << horloge.getHeureActuelle() << "h" << std::endl;
+
+		// Attendre la durée d'avancement
+		std::this_thread::sleep_for(dureeAvancement);
+
+		// Sortir de la boucle si l'heure actuelle est 23h
+		if (horloge.getHeureActuelle() == 23) {
+			break;
+		}
+	}
+}
+
 int main() {
-	Horloge horloge; // Créer une instance de la classe Horloge
+	Horloge horloge;
 
-	std::cout << "Heure actuelle : " << horloge.getHeureActuelle() << "h" << std::endl;
+	// Créer un thread pour faire avancer le temps
+	std::thread threadAvancement(avancerTemps, std::ref(horloge));
 
-	// Avancer le temps de 8 heures
-	horloge.avancerTemps(8);
-
-	std::cout << "Heure actuelle : " << horloge.getHeureActuelle() << "h" << std::endl;
-
-	// Avancer le temps de 18 heures
-	horloge.avancerTemps(18);
-
-	std::cout << "Heure actuelle : " << horloge.getHeureActuelle() << "h" << std::endl;
+	// Attendre que le thread de l'avancement du temps se termine
+	threadAvancement.join();
 
 	return 0;
 }
